7b24f68aed333ac3ce554d783954865abf82997b
bilal-dallali <dallali-bilal@hotmail.fr>
2024-10-25 17:41:30 +0200

    Initial Commit

---
+++ Cookpedia/CookpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookpediaApp.swift
+//  Cookpedia
+//
+//  Created by Bilal Dallali on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
