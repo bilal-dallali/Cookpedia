c6cc10975a1d8b3588f0c41bf3d009de7804c9c4
bilal-dallali <dallali-bilal@hotmail.fr>
2024-11-01 12:11:54 +0100

    notifications at this time

--- Cookpedia/View/NotificationPageView.swift
+++ Cookpedia/View/NotificationPageView.swift
@@ -8,19 +8,122 @@
 import SwiftUI
 
 struct NotificationPageView: View {
+    
+    @State private var isGeneralSelected: Bool = true
+    @State private var isSystemSelected: Bool = false
+    
     var body: some View {
-        VStack {
-            Text("Notification")
+        VStack(spacing: 24) {
+            HStack(spacing: 0) {
+                Button {
+                    isGeneralSelected = true
+                    isSystemSelected = false
+                } label: {
+                    VStack(spacing: 12) {
+                        if isGeneralSelected {
+                            Text("General")
+                                .foregroundStyle(Color("Primary900"))
+                                .font(.custom("Urbanist-SemiBold", size: 18))
+                            Divider()
+                                .overlay {
+                                    Rectangle()
+                                        .fill(Color("Primary900"))
+                                        .frame(height: 4)
+                                        .clipShape(.rect(cornerRadius: 100))
+                                }
+                        } else {
+                            Text("General")
+                                .foregroundStyle(Color("Greyscale700"))
+                                .font(.custom("Urbanist-SemiBold", size: 18))
+                            Divider()
+                                .overlay {
+                                    Rectangle()
+                                        .fill(Color("Dark4"))
+                                        .frame(height: 2)
+                                        .clipShape(.rect(cornerRadius: 100))
+                                }
+                        }
+                    }
+                }
+                
+                Button {
+                    isGeneralSelected = false
+                    isSystemSelected = true
+                } label: {
+                    VStack(spacing: 12) {
+                        if isSystemSelected {
+                            Text("System")
+                                .foregroundStyle(Color("Primary900"))
+                                .font(.custom("Urbanist-SemiBold", size: 18))
+                            Divider()
+                                .overlay {
+                                    Rectangle()
+                                        .fill(Color("Primary900"))
+                                        .frame(height: 4)
+                                        .clipShape(.rect(cornerRadius: 100))
+                                }
+                        } else {
+                            Text("System")
+                                .foregroundStyle(Color("Greyscale700"))
+                                .font(.custom("Urbanist-SemiBold", size: 18))
+                            Divider()
+                                .overlay {
+                                    Rectangle()
+                                        .fill(Color("Dark4"))
+                                        .frame(height: 2)
+                                        .clipShape(.rect(cornerRadius: 100))
+                                }
+                        }
+                    }
+                }
+                
+            }
+            .frame(height: 41)
+            .frame(maxWidth: .infinity)
+            ScrollView {
+                VStack(spacing: 60) {
+                    Image("empty")
+                        .resizable()
+                        .frame(width: 280, height: 273)
+                    VStack(spacing: 12) {
+                        Text("Empty")
+                            .foregroundStyle(Color("MyWhite"))
+                            .font(.custom("Urbanist-Bold", size: 24))
+                        Text("You don't have any notification at this time")
+                            .foregroundStyle(Color("MyWhite"))
+                            .font(.custom("Urbanist-Regular", size: 18))
+                    }
+                }
+                .padding(.vertical, 140)
+            }
+            
         }
+        .padding(.horizontal, 24)
         .ignoresSafeArea(edges: .bottom)
         .background(Color("Dark1"))
         .navigationBarBackButtonHidden(true)
         .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
+            ToolbarItem(placement: .topBarLeading) {
                 BackButtonView()
             }
             ToolbarItem(placement: .principal) {
-                Text("Notification")
+                HStack {
+                    Text("Notification")
+                        .foregroundStyle(Color("MyWhite"))
+                        .font(.custom("Urbanist-Bold", size: 24))
+                        .padding(.leading, 16)
+                    Spacer()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    //
+                } label: {
+                    Image("setting")
+                        .resizable()
+                        .frame(width: 28, height: 28)
+                }
+
             }
         }
     }
