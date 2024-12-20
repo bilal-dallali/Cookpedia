2f5e47e533143d9fcdf90d8e1cec03f302aa6e4a
bilal-dallali <dallali-bilal@hotmail.fr>
2024-12-20 18:33:44 +0100

    mise en page recipe cover photo

--- Cookpedia/View/RecipeDetailsView.swift
+++ Cookpedia/View/RecipeDetailsView.swift
@@ -23,7 +23,7 @@ struct RecipeDetailsView: View {
     init(recipeId: Int) {
         self.recipeId = recipeId
         self.recipeDetails = RecipeDetails(
-            id: recipeId, // Placeholder, replace with actual loading logic
+            id: recipeId,
             userId: 0,
             title: "Loading...",
             recipeCoverPictureUrl1: "",
@@ -41,94 +41,84 @@ struct RecipeDetailsView: View {
     }
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 24) {
-                AsyncImage(url: URL(string: "\(baseUrl)/recipes/recipe-cover/\(recipeDetails.recipeCoverPictureUrl1).jpg")) { image in
-                    image
-                        .resizable()
-                        .frame(width: 72, height: 72)
-                        .clipShape(RoundedRectangle(cornerRadius: .infinity))
-                } placeholder: {
-                    ProgressView()
-                        .frame(width: 72, height: 72)
-                }
-                
-                Text("\(recipeDetails.title)")
-                
-                AsyncImage(url: URL(string: "\(baseUrl)/users/profile-picture/\(recipeDetails.profilePictureUrl).jpg")) { image in
-                    image
-                        .resizable()
-                        .frame(width: 72, height: 72)
-                        .clipShape(RoundedRectangle(cornerRadius: .infinity))
-                } placeholder: {
-                    ProgressView()
-                        .frame(width: 72, height: 72)
-                }
-                
-                Text(recipeDetails.fullName)
-                Text(recipeDetails.username)
-                Text(recipeDetails.description)
-                ForEach(recipeDetails.ingredients, id: \.index) { ingredient in
-                    HStack {
-                        Text("\(ingredient.index)")
-                        Text("\(ingredient.ingredient)")
-                    }
-                }
-                
-                ForEach(recipeDetails.instructions, id: \.index) { instruction in
-                    VStack {
-                        HStack {
-                            Text("\(instruction.index)")
-                            Text("\(instruction.instruction)")
-                        }
-                        HStack {
-                            AsyncImage(url: URL(string: "\(baseUrl)/recipes/instruction-image/\(instruction.instructionPictureUrl1 ?? "").jpg")) { image in
-                                image
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
-                            } placeholder: {
-                                Image("ellipse")
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
-                            }
-                            
-                            AsyncImage(url: URL(string: "\(baseUrl)/recipes/instruction-image/\(instruction.instructionPictureUrl2 ?? "").jpg")) { image in
-                                image
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
-                            } placeholder: {
-                                Image("ellipse")
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
-                            }
-                            
-                            AsyncImage(url: URL(string: "\(baseUrl)/recipes/instruction-image/\(instruction.instructionPictureUrl3 ?? "").jpg")) { image in
-                                image
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
-                            } placeholder: {
-                                Image("ellipse")
-                                    .resizable()
-                                    .frame(width: 72, height: 72)
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 24) {
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 0) {
+                                AsyncImage(url: URL(string: "\(baseUrl)/recipes/recipe-cover/\(recipeDetails.recipeCoverPictureUrl1).jpg")) { image in
+                                    image
+                                        .resizable()
+                                        .frame(width: geometry.size.width, height: 430)
+                                } placeholder: {
+                                    Rectangle()
+                                        .fill(Color("Greyscale400"))
+                                        .frame(width: geometry.size.width, height: 430)
+                                }
+                                
+                                AsyncImage(url: URL(string: "\(baseUrl)/recipes/recipe-cover/\(recipeDetails.recipeCoverPictureUrl2).jpg")) { image in
+                                    image
+                                        .resizable()
+                                        .frame(width: geometry.size.width, height: 430)
+                                } placeholder: {
+                                    Rectangle()
+                                        .fill(Color("Greyscale400"))
+                                        .frame(width: geometry.size.width, height: 430)
+                                }
                             }
                         }
+                        .scrollIndicators(.hidden)
+                        .scrollTargetBehavior(.paging)
+                        
+                
                     }
                 }
-
+                .scrollIndicators(.hidden)
             }
-        }
-        .navigationBarBackButtonHidden(false)
-        .onAppear {
-            apiGetManager.getRecipeDetails(recipeId: recipeId) { result in
-                DispatchQueue.main.async {
-                    switch result {
-                        case .success(let details):
-                            print("ghjk")
-                            self.recipeDetails = details
-                            //self.isLoading = false
-                            //self.recipeCoverPictureUrl1 = details.recipeCoverPictureUrl1
-                        case .failure(let error):
-                            print("error \(error.localizedDescription)")
+            .ignoresSafeArea(edges: .all)
+            .background(Color("Dark1"))
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    BackButtonView()
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 20) {
+                        Button {
+                            //
+                        } label: {
+                            Image("bookmark-unselected")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                        }
+                        Button {
+                            //
+                        } label: {
+                            Image("send")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                        }
+                        Button {
+                            //
+                        } label: {
+                            Image("more-circle")
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                        }
+                    }
+
+                }
+            }
+            .onAppear {
+                apiGetManager.getRecipeDetails(recipeId: recipeId) { result in
+                    DispatchQueue.main.async {
+                        switch result {
+                            case .success(let details):
+                                self.recipeDetails = details
+                            case .failure(let error):
+                                print("error \(error.localizedDescription)")
+                        }
                     }
                 }
             }
