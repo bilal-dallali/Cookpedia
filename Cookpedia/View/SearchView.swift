5b13008a7a76954c0978ce4fdccdaed18c34a52e
bilal-dallali <dallali-bilal@hotmail.fr>
2025-03-08 16:06:54 +0100

    search recipe textfield

--- Cookpedia/View/SearchView.swift
+++ Cookpedia/View/SearchView.swift
@@ -15,65 +15,119 @@ struct SearchView: View {
     @State private var isPeopleSelected: Bool = false
     
     var body: some View {
-        VStack {
-            ScrollView {
-                Text("Search view")
-            }
-            .scrollIndicators(.hidden)
-        }
-        .background(Color("Dark1"))
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                BackButtonView()
-                    .padding(.top, 16)
-            }
-            ToolbarItem(placement: .principal) {
-                HStack(spacing: 12) {
-                    Image("Search - Regular - Light - Outline")
+        VStack(spacing: 24) {
+            HStack(spacing: 12) {
+                Image("Search - Regular - Light - Outline")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color("MyWhite"))
+                    .padding(.leading, 20)
+                TextField("", text: $searchText)
+                    .foregroundStyle(Color("MyWhite"))
+                    .font(.custom("Urbanist-Semibold", size: 16))
+                    .keyboardType(.default)
+                    .multilineTextAlignment(.leading)
+                    .frame(height: 22)
+                    .frame(maxWidth: .infinity)
+                    .focused($isTextFocused)
+                    .submitLabel(.done)
+                    .onSubmit {
+                        //
+                    }
+                Button {
+                    searchText = ""
+                } label: {
+                    Image("Icon=times, Component=Additional Icons")
                         .resizable()
                         .frame(width: 20, height: 20)
                         .foregroundStyle(Color("MyWhite"))
-                        .padding(.leading, 20)
-                    TextField("", text: $searchText)
-                        .foregroundStyle(Color("MyWhite"))
-                        .font(.custom("Urbanist-Semibold", size: 16))
-                        .keyboardType(.default)
-                        .multilineTextAlignment(.leading)
-                        .frame(height: 22)
-                        .frame(maxWidth: .infinity)
-                        .focused($isTextFocused)
-                        .submitLabel(.done)
-                        .onSubmit {
-                            //
-                        }
-                    Button {
-                        searchText = ""
-                    } label: {
-                        Image("Icon=times, Component=Additional Icons")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .foregroundStyle(Color("MyWhite"))
-                    }
-                    .padding(.trailing, 20)
                 }
-                .frame(height: 58)
-                .frame(maxWidth: .infinity)
-                .background(Color("Dark2"))
-                .clipShape(RoundedRectangle(cornerRadius: 16))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(isTextFocused ? Color("Primary900") : Color.clear, lineWidth: 2)
-                }
-                .padding(.top, 16)
+                .padding(.trailing, 20)
             }
+            .frame(height: 58)
+            .frame(maxWidth: .infinity)
+            .background(Color("Dark2"))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .overlay {
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(isTextFocused ? Color("Primary900") : Color.clear, lineWidth: 2)
+            }
+            HStack(spacing: 12) {
+                Button {
+                    isRecipeSelected = true
+                    isPeopleSelected = false
+                } label: {
+                    if isRecipeSelected {
+                        Text("Recipes")
+                            .foregroundStyle(Color("MyWhite"))
+                            .font(.custom("Urbanist-Semibold", size: 16))
+                            .frame(height: 38)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("Primary900"))
+                            .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                    } else {
+                        Text("Recipes")
+                            .foregroundStyle(Color("Primary900"))
+                            .font(.custom("Urbanist-Semibold", size: 16))
+                            .frame(height: 38)
+                            .frame(maxWidth: .infinity)
+                            .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: .infinity)
+                                    .stroke(Color("Primary900"), lineWidth: 2)
+                            }
+                    }
+                }
+                
+                Button {
+                    isRecipeSelected = false
+                    isPeopleSelected = true
+                } label: {
+                    if isPeopleSelected {
+                        Text("People")
+                            .foregroundStyle(Color("MyWhite"))
+                            .font(.custom("Urbanist-Semibold", size: 16))
+                            .frame(height: 38)
+                            .frame(maxWidth: .infinity)
+                            .background(Color("Primary900"))
+                            .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                    } else {
+                        Text("People")
+                            .foregroundStyle(Color("Primary900"))
+                            .font(.custom("Urbanist-Semibold", size: 16))
+                            .frame(height: 38)
+                            .frame(maxWidth: .infinity)
+                            .clipShape(RoundedRectangle(cornerRadius: .infinity))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: .infinity)
+                                    .stroke(Color("Primary900"), lineWidth: 2)
+                            }
+                    }
+                }
+            }
+            
+            ScrollView {
+                Text("azerty")
+            }
+            .scrollIndicators(.hidden)
+
         }
+        .padding(.horizontal, 24)
+        .padding(.top, 24)
+        .background(Color("Dark1"))
         .onTapGesture {
             isTextFocused = false
         }
         .onAppear {
-            isTextFocused = true
+            //isTextFocused = true
         }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                BackButtonView()
+            }
+        }
+        
     }
 }
 
